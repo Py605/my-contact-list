d6f3a3c0f31f1ff9ae679e938d02ce66f1f5c3c7
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2022-12-07 20:21:02 +0530

    changed something

--- app.js
+++ app.js
@@ -6,72 +6,69 @@ const Contact = require('./models/contact');
 
 const app = express();
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 let contactList = [
   {
-    name : "Priyanshu" ,
-    phone : "123456789"
+    name: 'Priyanshu',
+    phone: '123456789',
   },
   {
-    name : "py" ,
-    phone : "0987654321"
+    name: 'py',
+    phone: '0987654321',
   },
   {
-    name : "py605" ,
-    phone : "9343634936"
-  }
+    name: 'py605',
+    phone: '9343634936',
+  },
 ];
 
-app.get('/',function(req,res){
-  Contact.find({},function(err,contacts){
-    if(err)
-    {
-      console.log("error in fetching data from db.");
-    return;
+app.get('/', function (req, res) {
+  Contact.find({}, function (err, contacts) {
+    if (err) {
+      console.log('error in fetching data from db.');
+      return;
     }
 
-    return res.render("home",{
-      title : "My Contact List",
-      contactList : contacts
+    return res.render('home', {
+      title: 'My Contact List',
+      contactList: contacts,
     });
-  })
-  
-});
-
-app.post('/create-contact',function(req,res){
-  // contactList.push(req.body);
-  // res.redirect('/');
-  Contact.create({
-    name: req.body.name,
-    phone: req.body.phone
-  },function(err,newContact){
-    if(err)
-    {
-      console.log("error in creating this contact..");
-    return;
-    }
-    console.log("******",newContact);
-    return res.redirect('back');
   });
 });
 
-app.get('/delete-contact/:phone',function(req,res){
-  let phone = req.params.phone;
-  
-  Contact.deleteOne({phone:phone},function(err){
-    if(err)
+app.post('/create-contact', function (req, res) {
+  // contactList.push(req.body);
+  // res.redirect('/');
+  Contact.create(
     {
+      name: req.body.name,
+      phone: req.body.phone,
+    },
+    function (err, newContact) {
+      if (err) {
+        console.log('error in creating this contact..');
+        return;
+      }
+      console.log('******', newContact);
+      return res.redirect('back');
+    }
+  );
+});
+
+app.get('/delete-contact/:phone', function (req, res) {
+  let phone = req.params.phone;
+
+  Contact.deleteOne({ phone: phone }, function (err) {
+    if (err) {
       console.log('error in deleting an object from db');
       return;
     }
-   return res.redirect('/');
-  })
-  
+    return res.redirect('/');
+  });
 });
 
-
-app.listen('4000',function(){
+app.listen('4000', function () {
   console.log('Server started at port 4000 ');
 });
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/contacts_list');
+mongoose.connect('mongodb://localhost/27017', { useNewUrlParser: 'true' });
 
 const db = mongoose.connection;
 
-db.on('error',console.error.bind(console,'Having some issues while connecting to db...'));
+db.on(
+  'error',
+  console.error.bind(console, 'Having some issues while connecting to db...')
+);
 
-db.once('open',function(){
-  console.log('finally, it Worked, it Worked!!')
-})
+db.once('open', function () {
+  console.log('finally, it Worked, it Worked!!');
+});
