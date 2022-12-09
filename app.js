a29ab9b8a13ab03f147aa49c648fce33137fc60f
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2022-12-09 11:47:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.get('/delete-contact/:phone', function (req, res) {
     return res.redirect('/');
   });
 });
-
-app.listen('4000', function () {
+const port = process.env.PORT || 4000;
+app.listen(port, function () {
   console.log('Server started at port 4000 ');
 });
